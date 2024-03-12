d3e0b4c4a5b011ed2cfc1b1d29040a9f7b07305b
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-03-12 03:10:35 +0300

    5.1 Модель корзины

--- app/models.py
+++ app/models.py
@@ -7,6 +7,26 @@ from app.auth import pwd_context
 Base = declarative_base()
 
 
+class Cart(Base):
+    __tablename__ = "carts"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="cart")
+    items = relationship("CartItem", back_populates="cart")
+
+
+class CartItem(Base):
+    __tablename__ = "cart_items"
+    id = Column(Integer, primary_key=True, index=True)
+    cart_id = Column(Integer, ForeignKey("carts.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer)
+
+    cart = relationship("Cart", back_populates="items")
+    product = relationship("Product")
+
+
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
