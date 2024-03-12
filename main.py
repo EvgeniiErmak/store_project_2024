a9fb0199eae4a193e44860f3bc97902be9ea732a
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-03-12 05:15:05 +0300

    7. Запуск приложения

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from app.api import api_router
 
 app = FastAPI()
 
+# Include routers
 app.include_router(api_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
